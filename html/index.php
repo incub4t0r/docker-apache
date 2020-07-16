4bc7e8959cbafb36e2c5c4675fcb4317ccce643e
Debian <admin@ip-172-31-14-247.us-east-2.compute.internal>
2020-07-16 05:12:13 +0000

    initial commit

---
+++ html/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		43y3s EC2 Apache
+	</title>
+	<style>
+		body{
+			background-image: url('blurredbackground.jpg');	
+			background-repeat: no-repeat;
+			background-size: cover;
+			background-attachment: fixed;
+		}
+		video {
+			height: 100%
+			width: 100%
+		}
+	</style>		
+</head>
+
+	<body>
+		<h1>
+			Hello World!
+		</h1>
+	<p>
+		This is a test of a docker container running an apache php webserver!
+	</p>
+	<p>
+		Also Sarah if you're reading this get rekt, but i promise the video is swf, may be a little fire tho
+	</p>
+	<video controls>
+		<source src="elevatortrap.mp4" type="video/mp4">
+	</video>
+	</body>	
+</html>
